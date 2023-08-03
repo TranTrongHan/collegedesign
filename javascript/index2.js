497dc64e3cf62b1bdb3f4ada3e606eaf72fda604
TranTrongHan <139723233+TranTrongHan@users.noreply.github.com>
2023-08-03 22:06:06 +0700

    3/8luc10h5p

--- javascript/index2.js
+++ javascript/index2.js
@@ -7,16 +7,24 @@ submitBtn.onclick = function() {
 function showmodalForm(){
    var mail=document.querySelector("#email").value;
    if (mail === '') {
-        let model = document.querySelector('.modal-container')
+        let model = document.querySelector('.modal-error-container')
+        model.classList.remove('display');
+    }
+    else if(mail!==''){
+        let model=document.querySelector('.modal-succes-container')
         model.classList.remove('display');
     }
 }
 
 let tag=document.querySelector('.close-tag');
-let clickOutSide=document.querySelector('.form-container');
 tag.onclick=function(){
-    let model = document.querySelector('.modal-container')
+    let model = document.querySelector('.modal-error-container')
     model.classList.add('display');
 }
 
-
+let tag2=document.querySelector('.close-tag-2')
+console.log(tag2);
+tag2.onclick=function(){
+    let model = document.querySelector('.modal-succes-container')
+    model.classList.add('display');
+}

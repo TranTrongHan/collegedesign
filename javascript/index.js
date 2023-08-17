0a2643218226dc49abea2991e6c5f8717b989cda
TranTrongHan <139723233+TranTrongHan@users.noreply.github.com>
2023-08-17 18:28:38 +0700

    index3 update

--- javascript/index.js
+++ javascript/index.js
@@ -1,30 +1,32 @@
-// window.onscroll=function(){
-//     var navbar=document.getElementById("subbarId");
-//     var gototop=document.getElementById("gototop");
-//     var header=document.getElementById("myheadbar");
-//     if(document.documentElement.scrollTop>100||document.body.scrollTop>0){
-//         header.style.position="fixed";
-//         header.style.top="0";
-//         header.style.right=0;
-//         header.style.left=0;
-//         header.style.zIndex=9;
-//         gototop.style.display="block"
-//     }   
-//     else{
-//         header.style.position="relative"
-//         gototop.style.display="none "
-//     }
-// }
+window.onscroll=function(){
+    var navbar=document.getElementById("subbarId");
+    var gototop=document.getElementById("gototop");
+    var header=document.getElementById("myheadbar");
+    if(document.documentElement.scrollTop>100||document.body.scrollTop>0){
+        header.style.position="fixed";
+        header.style.opacity="0.7";
+        header.style.top="0";
+        header.style.right=0;
+        header.style.left=0;
+        header.style.zIndex=9;
+        gototop.style.display="block"
+    }   
+    else{
+        header.style.position="relative"
+        gototop.style.display="none "
+        header.style.opacity="unset";
+    }
+}
 
 
-// function goToTop(){
-//     var timer=setInterval(function(){
-//         document.documentElement.scrollTop-=20;
-//         if(document.documentElement.scrollTop<=0){
-//                clearInterval(timer);
-//         }
-//     },1);
-//}
+function goToTop(){
+    var timer=setInterval(function(){
+        document.documentElement.scrollTop-=20;
+        if(document.documentElement.scrollTop<=0){
+               clearInterval(timer);
+        }
+    },1);
+}
 
 
     
